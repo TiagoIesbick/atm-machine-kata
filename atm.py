0d49e514d4c629335231d6d107c789f0d485ab71
TiagoIesbick <tiagoiesbick@gmail.com>
2025-11-13 01:23:32 -0300

    Refactor ATMService to accept custom values and types, add validation methods for input integrity, and enhance test coverage for initialization and validation logic.

--- atm.py
+++ atm.py
@@ -1,12 +1,40 @@
 import pandas as pd
 
 
+class ATMServiceError(Exception):
+    """Base ATM Service Error"""
+
+
 class ATMService:
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        values: list[int] = [500, 200, 100, 50, 20, 10, 5, 2, 1],
+        values_type: list[str] = ["bill", "bill", "bill", "bill", "bill", "bill", "bill", "coin", "coin"]
+    ) -> None:
+
+        self._validate_list_of_ints(values)
+        self._validate_list_of_bills_or_coins(values_type)
+        self._validate_lists_lenght(values, values_type)
+
         self.initial_state = pd.DataFrame({
-            "Values": [500, 200, 100, 50, 20, 10, 5, 2, 1],
-            "Type": ["bill", "bill", "bill", "bill", "bill", "bill", "bill", "coin", "coin" ]
-        })
+            "Values": values,
+            "Type": values_type
+        }).sort_values(by=["Values"], ascending=False, ignore_index=True)
+
+    def _validate_list_of_ints(self, int_list: list[int]) -> bool:
+        if not (isinstance(int_list, list) and all(isinstance(i, int) for i in int_list)):
+            raise ValueError("The list of values must contain only integers.")
+        return True
+
+    def _validate_list_of_bills_or_coins(self, money_type_list: list[str]) -> bool:
+        if not (isinstance(money_type_list, list) and all(i in {"bill", "coin"} for i in set(money_type_list))):
+            raise ValueError("The list of types must contain only 'bill' or 'coin'.")
+        return True
+
+    def _validate_lists_lenght(self, list1: list, list2: list) -> bool:
+        if not len(list1) == len(list2):
+            raise ATMServiceError("The list of values must be the same length as the list of types.")
+        return True
 
     def withdraw(self, amount: int):
         for i in self.initial_state["Values"]:
@@ -16,16 +44,6 @@ class ATMService:
                 print(f"{qut} {self.initial_state.loc[self.initial_state["Values"] == i, "Type"].values[0]}{'s'[:qut^1]} of {i}")
 
 
-atm = ATMService()
-atm.withdraw(434)
+# atm = ATMService()
 
-amount = 434
-
-# for i in atm.initial_state["Values"]:
-#     if i > amount:
-#         pass
-#     elif i < amount:
-#         qut = amount // i
-#         amount -= qut*i
-#         print(qut, amount)
-#         print(f"{qut} {atm.initial_state.loc[atm.initial_state["Values"] == i, "Type"].values[0]}")
+# atm.withdraw(434)
--- test_atm.py
+++ test_atm.py
@@ -1,6 +1,35 @@
-from atm import ATMMachine, ATMService
+import pytest
+import pandas as pd
+from atm import ATMService, ATMServiceError
 
 
 def test_init_atm_service():
-    atm_svc = ATMService()
-    assert atm_svc.initial_state ==
+    values = [5, 10, 3, 9]
+    values_type = ["bill", "coin", "bill", "bill"]
+    atm = ATMService(values, values_type)
+    assert atm.initial_state.equals(pd.DataFrame({
+        "Values": [10, 9, 5, 3],
+        "Type": ["coin", "bill", "bill", "bill"]
+    }))
+
+def test_validate_list_of_ints():
+    atm = ATMService()
+    with pytest.raises(ValueError):
+        atm._validate_list_of_ints([1, "a"])
+    with pytest.raises(ValueError):
+        atm._validate_list_of_ints("hello")
+    assert atm._validate_list_of_ints([1,2])
+
+def test_validate_list_of_bills_or_coins():
+    atm = ATMService()
+    with pytest.raises(ValueError):
+        atm._validate_list_of_bills_or_coins(["bill", "coin", "hello"])
+    with pytest.raises(ValueError):
+        atm._validate_list_of_bills_or_coins("hello")
+    assert atm._validate_list_of_bills_or_coins(["bill", "coin"])
+
+def test_validate_lists_lenght():
+    atm = ATMService()
+    with pytest.raises(ATMServiceError):
+        atm._validate_lists_lenght([1,2], [1])
+    assert atm._validate_lists_lenght([1,2], ["a", "b"])
