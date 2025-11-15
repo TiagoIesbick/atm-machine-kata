d235aeebd25df9a99474a4e457252977d1fb8208
TiagoIesbick <tiagoiesbick@gmail.com>
2025-11-15 20:12:44 -0300

    Add MoneyPydanticWithQuantity and InsufficientATMCashError classes; enhance ATMService with quantity management and validation
    - Introduced MoneyPydanticWithQuantity to track cash quantities.
    - Updated ATMServicePydantic to support withdrawals considering available quantities.
    - Added InsufficientATMCashError for better error handling.
    - Enhanced tests to validate new functionality and edge cases for quantity management.

--- with_pydantic/atm_pydantic.py
+++ with_pydantic/atm_pydantic.py
@@ -7,6 +7,14 @@ class MoneyPydantic(BaseModel):
     Type: Literal["bill", "coin"] = Field(description="Money type.")
 
 
+class MoneyPydanticWithQuantity(MoneyPydantic):
+    Quantity: int = Field(ge=0, description="Quantity of the type of money available.")
+
+
+class InsufficientATMCashError(Exception):
+    pass
+
+
 class ATMServicePydantic:
     def __init__(
         self,
@@ -20,9 +28,21 @@ class ATMServicePydantic:
             MoneyPydantic(Values=5, Type="bill"),
             MoneyPydantic(Values=2, Type="coin"),
             MoneyPydantic(Values=1, Type="coin")
+        ],
+        initial_state_with_quantity: list[MoneyPydanticWithQuantity] = [
+            MoneyPydanticWithQuantity(Values=500, Type="bill", Quantity=2),
+            MoneyPydanticWithQuantity(Values=200, Type="bill", Quantity=3),
+            MoneyPydanticWithQuantity(Values=100, Type="bill", Quantity=5),
+            MoneyPydanticWithQuantity(Values=50, Type="bill", Quantity=12),
+            MoneyPydanticWithQuantity(Values=20, Type="bill", Quantity=20),
+            MoneyPydanticWithQuantity(Values=10, Type="bill", Quantity=50),
+            MoneyPydanticWithQuantity(Values=5, Type="bill", Quantity=100),
+            MoneyPydanticWithQuantity(Values=2, Type="coin", Quantity=250),
+            MoneyPydanticWithQuantity(Values=1, Type="coin", Quantity=500)
         ]
     ) -> None:
         self.initial_state = sorted(initial_state, key=lambda x: x.Values, reverse=True)
+        self.initial_state_with_quantity = sorted(initial_state_with_quantity, key=lambda x: x.Values, reverse=True)
 
     def withdraw(self, amount: int) -> str:
         withdraw_statement = []
@@ -32,3 +52,20 @@ class ATMServicePydantic:
                 amount -= qut * money.Values
                 withdraw_statement.append(f"{qut} {money.Type}{'s'[:qut^1]} of {money.Values}.")
         return '\n'.join(withdraw_statement)
+
+    def withdraw_with_quantity(self, amount: int) -> str:
+        self._validate_amount_available(amount)
+        withdraw_statement = []
+        for money in self.initial_state_with_quantity:
+            qut = min(amount // money.Values, money.Quantity)
+            if qut > 0:
+                amount -= qut * money.Values
+                money.Quantity -= qut
+                withdraw_statement.append(f"{qut} {money.Type}{'s'[:qut^1]} of {money.Values}.")
+        return '\n'.join(withdraw_statement)
+
+    def _validate_amount_available(self, amount: int) -> None:
+        if amount > sum(m.Values * m.Quantity for m in self.initial_state_with_quantity):
+            raise InsufficientATMCashError(
+                "The ATM machine has not enough money, please go to the nearest atm machine"
+            )
--- with_pydantic/test_atm_pydantic.py
+++ with_pydantic/test_atm_pydantic.py
@@ -1,5 +1,5 @@
 import pytest
-from atm_pydantic import ATMServicePydantic, MoneyPydantic
+from atm_pydantic import ATMServicePydantic, MoneyPydantic, MoneyPydanticWithQuantity, InsufficientATMCashError
 from pydantic import ValidationError
 
 
@@ -9,20 +9,66 @@ def test_init_atm_service():
         MoneyPydantic(Values=2, Type="coin"),
         MoneyPydantic(Values=50, Type="bill")
     ]
-    atm = ATMServicePydantic(initial_state=initial_state)
+    initial_state_with_quantity = [
+        MoneyPydanticWithQuantity(Values=2, Type="coin", Quantity=250),
+        MoneyPydanticWithQuantity(Values=50, Type="bill", Quantity=12),
+        MoneyPydanticWithQuantity(Values=100, Type="bill", Quantity=0)
+    ]
+    atm = ATMServicePydantic(
+        initial_state=initial_state,
+        initial_state_with_quantity=initial_state_with_quantity
+    )
     assert atm.initial_state == [
             MoneyPydantic(Values=50, Type="bill"),
             MoneyPydantic(Values=5, Type="bill"),
             MoneyPydantic(Values=2, Type="coin")
         ]
+    assert atm.initial_state_with_quantity == [
+        MoneyPydanticWithQuantity(Values=100, Type="bill", Quantity=0),
+        MoneyPydanticWithQuantity(Values=50, Type="bill", Quantity=12),
+        MoneyPydanticWithQuantity(Values=2, Type="coin", Quantity=250)
+    ]
     with pytest.raises(ValidationError):
         ATMServicePydantic(initial_state=[MoneyPydantic(Values=0, Type="bill")])
     with pytest.raises(ValidationError):
         ATMServicePydantic(initial_state=[MoneyPydantic(Values=-1, Type="bill")])
     with pytest.raises(ValidationError):
         ATMServicePydantic(initial_state=[MoneyPydantic(Values=5, Type="hello")])
+    with pytest.raises(ValueError):
+        ATMServicePydantic(
+            initial_state_with_quantity=MoneyPydanticWithQuantity(Values=100, Type="bill", Quantity=-1)
+        )
 
 def test_withdraw():
     atm = ATMServicePydantic()
     assert atm.withdraw(604) == '1 bill of 500.\n1 bill of 100.\n2 coins of 2.'
     assert atm.withdraw(434) == '2 bills of 200.\n1 bill of 20.\n1 bill of 10.\n2 coins of 2.'
+
+def test_withdraw_with_quantity():
+    atm = ATMServicePydantic()
+    assert atm.withdraw_with_quantity(1725) == '2 bills of 500.\n3 bills of 200.\n1 bill of 100.\n1 bill of 20.\n1 bill of 5.'
+    assert atm.initial_state_with_quantity == [
+        MoneyPydanticWithQuantity(Values=500, Type="bill", Quantity=0),
+        MoneyPydanticWithQuantity(Values=200, Type="bill", Quantity=0),
+        MoneyPydanticWithQuantity(Values=100, Type="bill", Quantity=4),
+        MoneyPydanticWithQuantity(Values=50, Type="bill", Quantity=12),
+        MoneyPydanticWithQuantity(Values=20, Type="bill", Quantity=19),
+        MoneyPydanticWithQuantity(Values=10, Type="bill", Quantity=50),
+        MoneyPydanticWithQuantity(Values=5, Type="bill", Quantity=99),
+        MoneyPydanticWithQuantity(Values=2, Type="coin", Quantity=250),
+        MoneyPydanticWithQuantity(Values=1, Type="coin", Quantity=500)
+    ]
+    assert atm.withdraw_with_quantity(1825) == '4 bills of 100.\n12 bills of 50.\n19 bills of 20.\n44 bills of 10.\n1 bill of 5.'
+    assert atm.initial_state_with_quantity == [
+        MoneyPydanticWithQuantity(Values=500, Type="bill", Quantity=0),
+        MoneyPydanticWithQuantity(Values=200, Type="bill", Quantity=0),
+        MoneyPydanticWithQuantity(Values=100, Type="bill", Quantity=0),
+        MoneyPydanticWithQuantity(Values=50, Type="bill", Quantity=0),
+        MoneyPydanticWithQuantity(Values=20, Type="bill", Quantity=0),
+        MoneyPydanticWithQuantity(Values=10, Type="bill", Quantity=6),
+        MoneyPydanticWithQuantity(Values=5, Type="bill", Quantity=98),
+        MoneyPydanticWithQuantity(Values=2, Type="coin", Quantity=250),
+        MoneyPydanticWithQuantity(Values=1, Type="coin", Quantity=500)
+    ]
+    with pytest.raises(InsufficientATMCashError):
+        atm.withdraw_with_quantity(1551)
