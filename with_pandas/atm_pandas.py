0531d6296870b2de4404ec98ae8a71fe7feeb41e
TiagoIesbick <tiagoiesbick@gmail.com>
2025-11-16 00:16:55 -0300

    Enhance ATMService to manage cash quantities and add withdrawal functionality with quantity tracking
    - Introduced InsufficientATMCashError for better error handling when cash is insufficient.
    - Updated ATMService to include quantity management for cash values.
    - Added new method withdraw_with_quantity to handle withdrawals considering available quantities.
    - Enhanced validation methods to ensure integrity of values, types, and quantities.
    - Updated tests to cover new functionality and edge cases for quantity management.

--- with_pandas/atm_pandas.py
+++ with_pandas/atm_pandas.py
@@ -1,39 +1,60 @@
 import pandas as pd
+from typing import Optional
 
 
 class ATMServiceError(Exception):
     """Base ATM Service Error"""
 
 
+class InsufficientATMCashError(ATMServiceError):
+    pass
+
+
 class ATMService:
     def __init__(
         self,
         values: list[int] = [500, 200, 100, 50, 20, 10, 5, 2, 1],
-        values_type: list[str] = ["bill", "bill", "bill", "bill", "bill", "bill", "bill", "coin", "coin"]
+        values_type: list[str] = ["bill"] * 7 + ["coin", "coin"],
+        values_qut: list[int] | None = [2, 3, 5, 12, 20, 50, 100, 250, 500]
     ) -> None:
 
-        self._validate_list_of_ints(values)
+        self._validate_list_of_values(values)
         self._validate_list_of_bills_or_coins(values_type)
-        self._validate_lists_lenght(values, values_type)
+        self._validate_list_of_quantities(values_qut)
+        self._validate_lists_lenght(values, values_type, values_qut)
+        self.values_qut = values_qut
 
         self.initial_state = pd.DataFrame({
             "Values": values,
             "Type": values_type
         }).sort_values(by=["Values"], ascending=False, ignore_index=True)
 
-    def _validate_list_of_ints(self, int_list: list[int]) -> bool:
-        if not (isinstance(int_list, list) and all(isinstance(i, int) and i > 0 for i in int_list)):
+        self.initial_state_with_quantity = pd.DataFrame({
+            "Values": values,
+            "Type": values_type,
+            "Quantity": values_qut
+        }).sort_values(by=["Values"], ascending=False, ignore_index=True)
+
+    def _validate_list_of_values(self, values_list: list[int]) -> bool:
+        if not (isinstance(values_list, list) and all(isinstance(i, int) and i > 0 for i in values_list)):
             raise ValueError("The list of values must contain only positive integers.")
         return True
 
-    def _validate_list_of_bills_or_coins(self, money_type_list: list[str]) -> bool:
-        if not (isinstance(money_type_list, list) and all(i in {"bill", "coin"} for i in set(money_type_list))):
+    def _validate_list_of_bills_or_coins(self, type_list: list[str]) -> bool:
+        if not (isinstance(type_list, list) and all(i in {"bill", "coin"} for i in set(type_list))):
             raise ValueError("The list of types must contain only 'bill' or 'coin'.")
         return True
 
-    def _validate_lists_lenght(self, list1: list, list2: list) -> bool:
-        if not len(list1) == len(list2):
+    def _validate_list_of_quantities(self, qut_list: Optional[list[int]] = None) -> bool:
+        if qut_list and not (isinstance(qut_list, list) and all(isinstance(i, int) and i >= 0 for i in qut_list)):
+            raise ValueError("The list of quantities must contain only integers equal to or greater than zero.")
+        return True
+
+    def _validate_lists_lenght(self, values_list: list[int], type_list: list[str], qut_list: Optional[list[int]] = None) -> None:
+        if not qut_list and len(values_list) != len(type_list):
             raise ATMServiceError("The list of values must be the same length as the list of types.")
+        if qut_list and not len(values_list) == len(type_list) == len(qut_list):
+            raise ATMServiceError("The list of values must be the same length as the list of types and the list of quantities.")
         return True
 
     def withdraw(self, amount: int) -> str:
@@ -46,3 +67,20 @@ class ATMService:
                     f"{qut} {row.Type}{'s'[:qut^1]} of {row.Values}."
                 )
         return '\n'.join(withdraw_statements)
+
+    def withdraw_with_quantity(self, amount: int) -> str:
+        self._validate_amount_available(amount)
+        withdraw_statement = []
+        for row in self.initial_state_with_quantity.itertuples():
+            qut = min(amount // row.Values, row.Quantity)
+            if qut > 0:
+                amount -= qut * row.Values
+                self.initial_state_with_quantity.loc[row.Index, "Quantity"] -= qut
+                withdraw_statement.append(f"{qut} {row.Type}{'s'[:qut^1]} of {row.Values}.")
+        return '\n'.join(withdraw_statement)
+
+    def _validate_amount_available(self, amount: int) -> None:
+        if not self.values_qut or amount > sum(self.initial_state_with_quantity["Values"] * self.initial_state_with_quantity["Quantity"]):
+            raise InsufficientATMCashError(
+                "The ATM machine has not enough money, please go to the nearest atm machine"
+            )
--- with_pandas/test_atm_pandas.py
+++ with_pandas/test_atm_pandas.py
@@ -1,28 +1,44 @@
 import pytest
 import pandas as pd
-from atm_pandas import ATMService, ATMServiceError
+from atm_pandas import ATMService, ATMServiceError, InsufficientATMCashError
 
 
 def test_init_atm_service():
     values = [5, 10, 3, 9]
     values_type = ["bill", "coin", "bill", "bill"]
-    atm = ATMService(values, values_type)
+    values_qut = [1, 0, 4, 6]
+    atm = ATMService(values, values_type, values_qut)
     assert atm.initial_state.equals(pd.DataFrame({
         "Values": [10, 9, 5, 3],
         "Type": ["coin", "bill", "bill", "bill"]
     }))
+    assert atm.initial_state_with_quantity.equals(pd.DataFrame({
+        "Values": [10, 9, 5, 3],
+        "Type": ["coin", "bill", "bill", "bill"],
+        "Quantity": [0, 6, 1, 4]
+    }))
 
-def test_validate_list_of_ints():
+def test_validate_list_of_values():
     atm = ATMService()
     with pytest.raises(ValueError):
-        atm._validate_list_of_ints([1, "a"])
+        atm._validate_list_of_values([1, "a"])
     with pytest.raises(ValueError):
-        atm._validate_list_of_ints("hello")
+        atm._validate_list_of_values("hello")
     with pytest.raises(ValueError):
-        atm._validate_list_of_ints([1, -1])
+        atm._validate_list_of_values([1, -1])
     with pytest.raises(ValueError):
-        atm._validate_list_of_ints([1, 0])
-    assert atm._validate_list_of_ints([1,2])
+        atm._validate_list_of_values([1, 0])
+    assert atm._validate_list_of_values([1,2])
+
+def test_validate_list_of_quantities():
+    atm = ATMService()
+    with pytest.raises(ValueError):
+        atm._validate_list_of_quantities([1, "a"])
+    with pytest.raises(ValueError):
+        atm._validate_list_of_quantities("hello")
+    with pytest.raises(ValueError):
+        atm._validate_list_of_quantities([1, -1])
+    assert atm._validate_list_of_quantities([1,0])
 
 def test_validate_list_of_bills_or_coins():
     atm = ATMService()
@@ -35,10 +51,32 @@ def test_validate_list_of_bills_or_coins():
 def test_validate_lists_lenght():
     atm = ATMService()
     with pytest.raises(ATMServiceError):
-        atm._validate_lists_lenght([1,2], [1])
+        atm._validate_lists_lenght([1,2], ["a"])
     assert atm._validate_lists_lenght([1,2], ["a", "b"])
+    with pytest.raises(ATMServiceError):
+        atm._validate_lists_lenght([1,2], ["a", "b"], [1])
+    assert atm._validate_lists_lenght([1,2], ["a", "b"], [1,2])
 
 def test_withdraw():
     atm = ATMService()
     assert atm.withdraw(604) == '1 bill of 500.\n1 bill of 100.\n2 coins of 2.'
     assert atm.withdraw(434) == '2 bills of 200.\n1 bill of 20.\n1 bill of 10.\n2 coins of 2.'
+
+def test_withdraw_with_quantity():
+    atm = ATMService()
+    assert atm.withdraw_with_quantity(1725) == '2 bills of 500.\n3 bills of 200.\n1 bill of 100.\n1 bill of 20.\n1 bill of 5.'
+    assert atm.initial_state_with_quantity.equals(pd.DataFrame({
+        "Values": [500, 200, 100, 50, 20, 10, 5, 2, 1],
+        "Type": ["bill"] * 7 + ["coin", "coin"],
+        "Quantity": [0, 0, 4, 12, 19, 50, 99, 250, 500]
+    }))
+    assert atm.withdraw_with_quantity(1825) == '4 bills of 100.\n12 bills of 50.\n19 bills of 20.\n44 bills of 10.\n1 bill of 5.'
+    assert atm.initial_state_with_quantity.equals(pd.DataFrame({
+        "Values": [500, 200, 100, 50, 20, 10, 5, 2, 1],
+        "Type": ["bill"] * 7 + ["coin", "coin"],
+        "Quantity": [0] * 5 + [6, 98, 250, 500]
+    }))
+    with pytest.raises(InsufficientATMCashError):
+        atm.withdraw_with_quantity(1551)
+    with pytest.raises(InsufficientATMCashError):
+        ATMService(values_qut=None).withdraw_with_quantity(1)
